f800ece77f1cef654a5e5da58f1569721879b055
Jaden Sailer <jadennsailer@gmail.com>
2020-08-27 20:27:37 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative './spec_helper'
-
-say_hello(Gabriela)
-
-  it 'defaults to Ruby Programmer when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-    say_hello()
-  end
-end
